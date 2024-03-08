ec4eeff966b68b563cc5c4adad0063545a5056cb
Tiago Gomes <tidgomes@gmail.com>
2024-03-08 10:27:56 -0300

    refactor Task layout

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,8 +1,15 @@
 export function Task({ id, text, done, deleteTask, doneTask }) {
   return (
-    <li>
-      <input type='checkbox' name='done' checked={done} onChange={() => doneTask(id)} />
-      <h2>{text}</h2>
+    <li className='singleTask'>
+      <span className='taskInfo'>
+        <input
+          type='checkbox'
+          name='done'
+          checked={done}
+          onChange={() => doneTask(id)}
+        />
+        <h2>{text}</h2>
+      </span>
       <button onClick={() => deleteTask(id)}>Delete</button>
     </li>
   )
